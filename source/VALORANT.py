4078acfa2b6d953ed30d711b3ba6a56f9b4ae6af
Prx001 <67240789+Prx001@users.noreply.github.com>
2021-07-11 12:57:54 +0430

    Clearify

--- source/VALORANT.py
+++ source/VALORANT.py
@@ -1,9 +1,13 @@
 import sys
+
+from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QDesktopWidget
+
 from Widget1 import Ui_Form as Widget_1
 from Widget2 import Ui_Form as Widget_2
 from Widget3 import Ui_Form as Widget_3
-from PyQt5.QtCore import Qt, QPoint
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QDesktopWidget
+
+
 class Form(QMainWindow):
 	def __init__(self):
 		super().__init__()
@@ -18,6 +22,7 @@ class Form(QMainWindow):
 		self.right = QWidget(self)
 		self.right.move(400, 0)
 		self.initUI()
+
 	def initUI(self):
 		self.resize(1280, 720)
 		self.move_to_center()
@@ -26,17 +31,22 @@ class Form(QMainWindow):
 		self.widget1.setupUi(self.left_1)
 		self.widget3.setupUi(self.right)
 		self.show()
+
 	def mousePressEvent(self, event):
 		self.oldPos = event.globalPos()
+
 	def mouseMoveEvent(self, event):
 		delta = QPoint(event.globalPos() - self.oldPos)
 		self.move(self.x() + delta.x(), self.y() + delta.y())
 		self.oldPos = event.globalPos()
+
 	def move_to_center(self):
 		qr = self.frameGeometry()
 		cp = QDesktopWidget().availableGeometry().center()
 		qr.moveCenter(cp)
 		self.move(qr.topLeft())
+
+
 app = QApplication(sys.argv)
 form = Form()
 sys.exit(app.exec_())
